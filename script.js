a6644403c286e5af546c733b6eff906971b14428
Faith Mansilungan <faith0094@stud.kea.dk>
2022-06-14 04:52:53 +0200

    Responsive forside + små-ændringer

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ function toggleMenu() {
   if (menuShown) {
     console.log(menuShown); // se i konsollen
     // sæt btn.textContent til "Luk", hvis menuShown er "true"
-    btn.textContent = "X";
+    btn.textContent = "LUK";
   } else {
     console.log(menuShown); // se i konsollen
     // sæt btn.textContent til "Menu", hvis menuShown er "false"
-    btn.textContent = "Menu";
+    btn.textContent = "MENU";
   }
   // "toggleMenu()" slutter her
 }
